5f7b10602d477072f47c517c9c2c26b888b5bd3a
roberthsu2003 <roberthsu2003@gmail.com>
2024-10-11 14:59:22 +0800

    lesson3_09.py

--- lesson3/lesson3_3.py
+++ lesson3/lesson3_3.py
@@ -1,3 +1,14 @@
 import tools
 
 print(tools.SUN)
+p1 = tools.Person(name="robert",age=30)
+print(p1)
+
+s1 = tools.Student(name="徐國堂",age=40,chinese=87,english=75,math=67)
+print(s1.name)
+print(s1.age)
+print(s1.english)
+print(s1.math)
+print(s1.chinese)
+print(s1.total)
+print(s1.average())
