114f5c347459a1f2ffdec33a86f3203f6109fc66
roberthsu2003 <roberthsu2003@gmail.com>
2024-10-18 14:59:07 +0800

    modify

--- lesson4/lesson4_4.py
+++ lesson4/lesson4_4.py
@@ -7,11 +7,16 @@ class Window(tk.Tk):
         self.title('使用ttk的套件')
         self.geometry('400x300')
         style = ttk.Style(self)
+        '''
         style.configure('TLabel',font=('Helvetica', 11)) #修改現有的
         style.configure('Title.TLabel',font=('Helvetica', 15),background="lightblue",foreground='red') #自訂的style
         message = ttk.Label(self,text='使用ttk的Label',style='Title.TLabel') #使用自訂的
         print(message.winfo_class())
         message.pack()
+        '''
+        style.configure('Main.TButton',font=('Arial',15))
+        btn1 = ttk.Button(self,text="Button Demo",style='Main.TButton')
+        btn1.pack(ipadx=10,ipady=20)
 
 def main():
     window = Window()
