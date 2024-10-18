56051d469f72b1bec30cc3205a7db37bda1c656f
roberthsu2003 <roberthsu2003@gmail.com>
2024-10-18 14:14:28 +0800

    lesson4_09

--- lesson4/lesson4_4.py
+++ lesson4/lesson4_4.py
@@ -6,7 +6,11 @@ class Window(tk.Tk):
         super().__init__(**kwargs)
         self.title('使用ttk的套件')
         self.geometry('400x300')
+        style = ttk.Style(self)
+        style.configure('TLabel',font=('Helvetica', 15))
+
         message = ttk.Label(self,text='使用ttk的Label')
+        print(message.winfo_class())
         message.pack()
 
 def main():
