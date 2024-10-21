efb2b0e5806de139014a77f2984c1c3891065354
roberthsu2003 <roberthsu2003@gmail.com>
2024-10-21 14:14:31 +0800

    lesson5_05

--- lesson5/lesson5_1.py
+++ lesson5/lesson5_1.py
@@ -4,6 +4,7 @@ from ttkthemes import ThemedTk
 class Window(ThemedTk):
     def __init__(self,*args, **kwargs):
         super().__init__(*args, **kwargs)
+        self.title('登入')
         #==============style===============
         style = ttk.Style(self)
         style.configure('TopFrame.TLabel',font=('Helvetica',20))
@@ -18,7 +19,9 @@ class Window(ThemedTk):
         #==============end topFrame===============
 
         bottomFrame = ttk.Frame(self)
-        bottomFrame.pack()
+        ttk.Label(bottomFrame,text='UserName:').grid(column=0,row=0)
+        ttk.Label(bottomFrame,text='Password:').grid(row=1,column=0)
+        bottomFrame.pack(expand=True,fill='x',padx=20,pady=(0,20))
 
 
 def main():
