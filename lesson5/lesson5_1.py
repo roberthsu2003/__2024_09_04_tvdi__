2cf636c5d1e808fecc4ca935042b146e55f2e73d
roberthsu2003 <roberthsu2003@gmail.com>
2024-10-21 13:59:35 +0800

    lesson5_04

--- lesson5/lesson5_1.py
+++ lesson5/lesson5_1.py
@@ -4,11 +4,25 @@ from ttkthemes import ThemedTk
 class Window(ThemedTk):
     def __init__(self,*args, **kwargs):
         super().__init__(*args, **kwargs)
+        #==============style===============
+        style = ttk.Style(self)
+        style.configure('TopFrame.TLabel',font=('Helvetica',20))
+        #============end style===============
+        
+        #==============topFrame===============
 
+        topFrame = ttk.Frame(self)
+        ttk.Label(topFrame,text='個人資訊輸入',style='TopFrame.TLabel').pack()
+        topFrame.pack(padx=20,pady=20)
+        
+        #==============end topFrame===============
+
+        bottomFrame = ttk.Frame(self)
+        bottomFrame.pack()
 
 
 def main():
-    window = Window(theme="breeze")
+    window = Window(theme="arc")
     window.mainloop()
 
 if __name__ == '__main__':
