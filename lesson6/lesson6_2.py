09b308d9cddf49245ce34c9daf250342722f39d0
roberthsu2003 <roberthsu2003@gmail.com>
2024-10-28 15:13:34 +0800

    lesson6_07

--- lesson6/lesson6_2.py
+++ lesson6/lesson6_2.py
@@ -36,26 +36,25 @@ class Window(ThemedTk):
         # define columns
         columns = ('date', 'county', 'aqi', 'pm25','status','lat','lon')
 
-        tree = ttk.Treeview(bottomFrame, columns=columns, show='headings')
+        self.tree = ttk.Treeview(bottomFrame, columns=columns, show='headings')
 
         # define headings
-        tree.heading('date', text='日期')
-        tree.heading('county', text='縣市')
-        tree.heading('aqi', text='AQI')
-        tree.heading('pm25', text='PM25')
-        tree.heading('status',text='狀態')
-        tree.heading('lat', text='緯度')
-        tree.heading('lon', text='經度')
+        self.tree.heading('date', text='日期')
+        self.tree.heading('county', text='縣市')
+        self.tree.heading('aqi', text='AQI')
+        self.tree.heading('pm25', text='PM25')
+        self.tree.heading('status',text='狀態')
+        self.tree.heading('lat', text='緯度')
+        self.tree.heading('lon', text='經度')
 
-        tree.column('date', width=150,anchor="center")
-        tree.column('county', width=80,anchor="center")
-        tree.column('aqi', width=50,anchor="center")
-        tree.column('pm25', width=50,anchor="center")
-        tree.column('status', width=50,anchor="center")
-        tree.column('lat', width=100,anchor="center")
-        tree.column('lon', width=100,anchor="center")
-        tree.insert("", "end", values=('2024-10-28 09:00','屏東縣',17,6.5,'良好',22.260899,120.651472))
-
+        self.tree.column('date', width=150,anchor="center")
+        self.tree.column('county', width=80,anchor="center")
+        self.tree.column('aqi', width=50,anchor="center")
+        self.tree.column('pm25', width=50,anchor="center")
+        self.tree.column('status', width=50,anchor="center")
+        self.tree.column('lat', width=100,anchor="center")
+        self.tree.column('lon', width=100,anchor="center")
+        
         # generate sample data
         #contacts = []
         #for n in range(1, 100):
@@ -65,13 +64,17 @@ class Window(ThemedTk):
         #for contact in contacts:
         #    tree.insert('', tk.END, values=contact)
         
-        tree.pack(side='right')
+        self.tree.pack(side='right')
         bottomFrame.pack(expand=True,fill='x',padx=20,pady=(0,20),ipadx=10,ipady=10)
 
             #==============end bottomFrame===============
         
     def sitename_selected(self,event):
-        print(self.selected_site.get()) 
+        selected = self.selected_site.get()
+        selected_data = datasource.get_selected_data(selected)
+        for record in selected_data:
+            self.tree.insert("", "end", values=record)
+
     
         
 
