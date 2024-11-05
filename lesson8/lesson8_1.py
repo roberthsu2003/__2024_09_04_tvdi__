01441c94ba6863d27c917d731fe70e081ca1f557
roberthsu2003 <roberthsu2003@gmail.com>
2024-11-05 14:33:16 +0800

    lesson8_11

--- lesson8/lesson8_1.py
+++ lesson8/lesson8_1.py
@@ -5,7 +5,7 @@ import tkinter as tk
 from ttkthemes import ThemedTk
 from tkinter.messagebox import showinfo
 import view
-from PIL import ImageTk, Image
+
 
 class Window(ThemedTk):
     def __init__(self,*args, **kwargs):
@@ -29,13 +29,8 @@ class Window(ThemedTk):
         bottomFrame = ttk.Frame(self)
             #==============SelectedFrame===============        
         self.selectedFrame= ttk.Frame(self,padding=[10,10,10,10])
-        #增加refresh button
-        self.icon_image = Image.open("refresh.png")
-        self.icon_photo = ImageTk.PhotoImage(self.icon_image)
-        icon_button = view.ImageButton(self.selectedFrame,
-                                text='重新下載資料',
-                                image=self.icon_photo,
-                                )
+        #增加refresh button        
+        icon_button = view.ImageButton(self.selectedFrame)
         icon_button.pack()
 
         #combobox選擇城市      
