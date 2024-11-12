a7e3d5a632646ac1db608078cf2ed956be5251e7
roberthsu2003 <roberthsu2003@gmail.com>
2024-11-12 11:49:44 +0800

    modify

--- lesson9/view/__init__.py
+++ lesson9/view/__init__.py
@@ -1,2 +1,3 @@
 from .sitename_frame import SitenameFrame
 from .image_button import ImageButton
+from .item_dialog import MyCustomDialog
---
+++ lesson9/view/item_dialog.py
@@ -0,0 +1,6 @@
+import tkinter as tk
+from tkinter.simpledialog import Dialog
+
+class MyCustomDialog(Dialog):
+    pass
+
