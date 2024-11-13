454cdaa13ee4cb64a648639008f883cf8a45764c
roberthsu2003 <roberthsu2003@gmail.com>
2024-11-13 11:43:24 +0800

    lesson9_14

--- lesson9/view/item_dialog.py
+++ lesson9/view/item_dialog.py
@@ -18,14 +18,15 @@ class MyCustomDialog(Dialog):
         # 創建對話框主體。返回應具有初始焦點的控件。
         main_frame = ttk.Frame(master,borderwidth=1,relief='groove')
         canvas_left = tk.Canvas(main_frame,width=200,height=200)
-        canvas_left.create_oval(10, 10, 80, 80, outline="#f11",fill="#1f1", width=2)
-        canvas_left.create_text(200, 150, text="優",font=("Helvetica",24,"bold"),fill='blue')
-        canvas_left.pack(side='right')
+        canvas_left.create_rectangle(10,10,190,190,outline="#9E7A7A",width=2)
+        #canvas_left.create_oval(10, 10, 80, 80, outline="#f11",fill="#1f1", width=2)
+        canvas_left.create_text(100, 40, text=f'AQI:{self.status}',font=("Helvetica",24,"bold"),fill='#9E7A7A')
+        canvas_left.pack(side='left')
 
         canvas_right = tk.Canvas(main_frame,width=200,height=200)
         canvas_right.create_oval(10, 10, 80, 80, outline="#f11",fill="#1f1", width=2)
-        canvas_right.create_text(200, 150, text="優",font=("Helvetica",24,"bold"),fill='blue')
-        canvas_right.pack(side='left')
+        canvas_right.create_text(200, 150, text=self.status,font=("Helvetica",24,"bold"),fill='#9E7A7A')
+        canvas_right.pack(side='right')
         main_frame.pack(expand=True,fill='x')
 
     def apply(self):
