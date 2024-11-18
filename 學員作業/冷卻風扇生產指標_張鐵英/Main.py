f70506aa9accc1e053337e25ea1e025d607a4a20
Austin <austin.chang16@gmail.com>
2024-11-18 22:17:17 +0800

    add treeview

--- 學員作業/冷卻風扇生產指標_張鐵英/Main.py
+++ 學員作業/冷卻風扇生產指標_張鐵英/Main.py
@@ -33,17 +33,46 @@ class Window(ThemedTk):
         
         sales_list = datasource.get_sales()
         #self.selected_site = tk.StringVar()
-        print("Hello Tkinter and Python 2") 
-        
         self.selected_sales = tk.StringVar()
-        sitenames_cb = ttk.Combobox(self.selectedFrame, textvariable=self.selected_sales,values=sales_list,state='readonly')
+        customer_cb = ttk.Combobox(self.selectedFrame, textvariable=self.selected_sales,values=sales_list,state='readonly')
         self.selected_sales.set('請選擇業務')
-        sitenames_cb.bind('<<ComboboxSelected>>', self.selected_sales)
-        sitenames_cb.pack(anchor='n',pady=10)
-        self.sitenameFrame = None 
+        customer_cb.bind('<<ComboboxSelected>>', self.selected_sales)
+        customer_cb.pack(anchor='n',pady=10)
+        self.customerFrame = None 
         self.selectedFrame.pack(side='left',fill='y')
-        print("Hello Tkinter and Python 1") 
             #==============End SelectedFrame=============== 
+            
+            #==============RightFrame======================
+        rightFrame = ttk.LabelFrame(bottomFrame,text="產品生產指標",padding=[10,10,10,10])
+        #建立treeView
+        # define columns
+        print("Hello Tkinter and Python 2") 
+        columns = ('Sales ID', 'Sales Name', 'Customer ID','Order ID', 'Yield Rate','Thru_put','Order Date','Delivery Date','Factory')
+        self.tree = ttk.Treeview(rightFrame, columns=columns, show='headings')
+        # self.tree.bind('<<TreeviewSelect>>', self.item_selected)
+        # define headings
+        self.tree.heading('Sales ID', text='業務代號')
+        self.tree.heading('Sales Name', text='業務名稱')
+        self.tree.heading('Customer ID', text='客戶代號')
+        self.tree.heading('Order ID', text='訂單號碼')
+        self.tree.heading('Yield Rate', text='良率')
+        self.tree.heading('Thru_put',text='直通率')
+        self.tree.heading('Order Date', text='下單日期')
+        self.tree.heading('Delivery Date', text='交貨日期')
+        self.tree.heading('Factory', text='生產工廠')
+        self.tree.column('Sales ID', width=80,anchor="center")
+        self.tree.column('Sales Name', width=100,anchor="center")
+        self.tree.column('Customer ID', width=120,anchor="center")
+        self.tree.column('Order ID', width=80,anchor="center")
+        self.tree.column('Yield Rate', width=50,anchor="center")
+        self.tree.column('Thru_put', width=50,anchor="center")
+        self.tree.column('Order Date', width=100,anchor="center")
+        self.tree.column('Delivery Date', width=100,anchor="center")
+        self.tree.column('Factory', width=80, anchor="center")
+        self.tree.pack(side='right')
+        rightFrame.pack(side='right')
+        print("Hello Tkinter and Python 1") 
+            #==============End RightFRame==================      
             
         bottomFrame.pack()
         #==============end bottomFrame===============
