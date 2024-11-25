2367756766753b52d2e080a865a2b08b0e808941
De-Jung Tseng <djtseng.c@nycu.edu.tw>
2024-11-25 08:53:11 +0800

    1125

--- 學員作業/電影推薦系統-De-Jung_Tseng/Project_Watch_Movie/view/__init__.py
+++ 學員作業/電影推薦系統-De-Jung_Tseng/Project_Watch_Movie/view/__init__.py
@@ -1,7 +1,9 @@
 # __init__.py 代表外部資料夾為package
 # package from .file import class
-from .sitename_frame import SitenameFrame
 
-from .image_button import ImageButton 
 
 from .login_dialog import LoginDialog
+
+from .canvas import TopCanvas
+
+from .treeview import TreeViewWidget
