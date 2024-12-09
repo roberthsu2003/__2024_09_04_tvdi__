9801759f0f6c021304812896d2135e7394d545cd
roberthsu2003 <roberthsu2003@gmail.com>
2024-12-09 09:41:22 +0800

    lesson7_1_3

--- lesson17/lesson17_1.py
+++ lesson17/lesson17_1.py
@@ -5,10 +5,12 @@ df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/gapmi
 
 app = Dash(__name__)
 
-app.layout = [
+app.layout = html.Div(
+    [
     html.H1("Dash App的標題",style={"textAlign":'center'}),
     dcc.Dropdown(df.country.unique(),value='Taiwan',id='dropdown-selection')
-]
+    ])
+    
 
 if __name__ == '__main__':
     app.run(debug=True)
