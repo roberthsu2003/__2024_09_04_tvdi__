40df2027f5accd7bdb24cadfb803d4664d8816d7
roberthsu2003 <roberthsu2003@gmail.com>
2024-12-09 10:37:38 +0800

    lesson7_1_5

--- lesson17/lesson17_1.py
+++ lesson17/lesson17_1.py
@@ -1,4 +1,4 @@
-from dash import Dash,html,dcc,callback,Input, Output
+from dash import Dash,html,dcc,callback,Input, Output,dash_table
 import pandas as pd
 import plotly.express as px
 
@@ -10,11 +10,16 @@ app.layout = html.Div(
     [
     html.H1("Dash App的標題",style={"textAlign":'center'}),
     dcc.Dropdown(df.country.unique(),value='Taiwan',id='dropdown-selection'),
+    dash_table.DataTable(data=[],page_size=10,id='datatable'),
     dcc.Graph(id='graph-content')
     ])
 
 @callback(
-    Output('graph-content','figure'),
+    [
+        Output('graph-content','figure'),
+        Output('datatable','data')
+     
+    ],
     Input('dropdown-selection','value')
 )
 def update_graph(value):
