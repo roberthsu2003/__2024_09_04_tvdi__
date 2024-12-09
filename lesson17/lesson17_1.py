ffa3c5a74eef5bf0f18861fac960e5b298a2873c
roberthsu2003 <roberthsu2003@gmail.com>
2024-12-09 11:38:21 +0800

    lesson7_1_8

--- lesson17/lesson17_1.py
+++ lesson17/lesson17_1.py
@@ -11,21 +11,16 @@ app.layout = html.Div(
     html.H1("Dash App的標題",style={"textAlign":'center'}),
     dcc.RadioItems(['pop','lifeExp','gdpPercap'],value='pop',inline=True,id='radio_item'),
     dcc.Dropdown(df.country.unique(),value='Taiwan',id='dropdown-selection'),
-    dash_table.DataTable(data=[],page_size=10,id='datatable',columns=[
-        {'id':'country','name':'country'},
-        {'id':'year','name':'year'},
-        {'id':'pop','name':'pop'}
-        ]),
+    dash_table.DataTable(data=[],page_size=10,id='datatable',columns=[]),
     dcc.Graph(id='graph-content')
     ])
 
 #圖表顯示的事件
 @callback(    
-    Output('graph-content','figure'),     
-    [
-        Input('dropdown-selection','value'),
-        Input('radio_item','value')
-    ]
+    Output('graph-content','figure'),
+    Input('dropdown-selection','value'),
+    Input('radio_item','value')
+    
 )
 def update_graph(country_value,radio_value):
     dff = df[df.country == country_value]
@@ -41,12 +36,25 @@ def update_graph(country_value,radio_value):
 
 #表格顯示的事件
 @callback(    
-    Output('datatable','data'),     
-    Input('dropdown-selection','value')
+    Output('datatable','data'),
+    Output('datatable','columns'),    
+    Input('dropdown-selection','value'),
+    Input('radio_item','value') 
 )
-def update_table(value):
-    dff = df[df.country == value]
-    return dff.to_dict('records')
+def update_table(country_value,radio_value):
+    dff = df[df.country == country_value]
+    columns = [
+        {'id':'country','name':'country'},
+        {'id':'year','name':'year'}        
+    ]
+    if radio_value == 'pop':
+        columns.append({'id':'pop','name':'pop'})
+    elif radio_value == 'lifeExp':
+        columns.append({'id':'lifeExp','name':'lifeExp'})
+    elif radio_value == 'gdpPercap':
+        columns.append({'id':'gdpPercap','name':'gdpPercap'})
+
+    return dff.to_dict('records'),columns
     
 
 if __name__ == '__main__':
