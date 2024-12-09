127ca2e0d23363d494e666b5b5e87a1e14a464b2
roberthsu2003 <roberthsu2003@gmail.com>
2024-12-09 09:22:47 +0800

    lesson17_1_1

--- lesson17/lesson17_1.py
+++ lesson17/lesson17_1.py
@@ -1,4 +1,13 @@
+from dash import Dash,html,dcc
 import pandas as pd
 
 df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/gapminder_unfiltered.csv')
-df
+
+app = Dash(__name__)
+
+app.layout = [
+    html.H1("Dash App的標題",style={"textAlign":'center'})
+]
+
+if __name__ == '__main__':
+    app.run(debug=True)
