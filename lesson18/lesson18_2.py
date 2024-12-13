86fc4d68f8036fd05a630ff2c8c7445ca8d7310d
roberthsu2003 <roberthsu2003@gmail.com>
2024-12-13 10:48:03 +0800

    lesson18_2_3

--- lesson18/lesson18_2.py
+++ lesson18/lesson18_2.py
@@ -14,6 +14,14 @@ radio_data = [['pop','人口'],['lifeExp','平均壽命'],['gdpPercap','人均gd
 #selected要的資料
 selected_data = [{'value':value,'label':value} for value in df.country.unique()]
 
+data = [
+  {"date": "Mar 22", "Apples": 2890, "Oranges": 2338, "Tomatoes": 2452},
+  {"date": "Mar 23", "Apples": 2756, "Oranges": 2103, "Tomatoes": 2402},
+  {"date": "Mar 24", "Apples": 3322, "Oranges": 986, "Tomatoes": 1821},
+  {"date": "Mar 25", "Apples": 3470, "Oranges": 2108, "Tomatoes": 2809},
+  {"date": "Mar 26", "Apples": 3129, "Oranges": 1726, "Tomatoes": 2290}
+]
+
 app.layout = dmc.MantineProvider(
     [
     
@@ -69,34 +77,50 @@ app.layout = dmc.MantineProvider(
             direction={"base": "column", "sm": "row"},
             gap={"base": "sm", "sm": "lg"},
             justify={"base": "center"},
-            
-
         )
     ,
-    #dcc.Graph(id='graph-content')
+        #dcc.Graph(id='graph-content')
+        # dmc.Container(
+        #    dcc.Graph(id='graph-content') 
+        # )
+     
         dmc.Container(
-           dcc.Graph(id='graph-content') 
+           dmc.LineChart(
+            h=300,
+            dataKey="date",
+            data=data,
+            series = [
+                {"name": "Apples", "color": "indigo.6"},
+                {"name": "Oranges", "color": "blue.6"},
+                {"name": "Tomatoes", "color": "teal.6"}
+            ],
+            curveType="linear",
+            tickLine="xy",
+            withXAxis=False,
+            withDots=False,
+            ),
+            mt=50 
         )
+        
     ]
 )
 
 #圖表顯示的事件
-@callback(    
-    Output('graph-content','figure'),
-    Input('dropdown-selection','value'),
-    Input('radio_item','value')
-    
-)
-def update_graph(country_value,radio_value):
-    dff = df[df.country == country_value]
-    if radio_value == "pop":
-        title = f'{country_value}:人口成長圖表'
-    elif radio_value == "lifeExp":
-        title = f'{country_value}:預期壽命'
-    elif radio_value == 'gdpPercap':
-        title = f'{country_value}:人均GDP'
+# @callback(    
+#     Output('graph-content','figure'),
+#     Input('dropdown-selection','value'),
+#     Input('radio_item','value')
+# )
+# def update_graph(country_value,radio_value):
+#     dff = df[df.country == country_value]
+#     if radio_value == "pop":
+#         title = f'{country_value}:人口成長圖表'
+#     elif radio_value == "lifeExp":
+#         title = f'{country_value}:預期壽命'
+#     elif radio_value == 'gdpPercap':
+#         title = f'{country_value}:人均GDP'
 
-    return px.line(dff,x='year',y=radio_value,title=title)
+#     return px.line(dff,x='year',y=radio_value,title=title)
 
 #表格顯示的事件
 @callback(    
