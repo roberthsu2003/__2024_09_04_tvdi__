48ebcee43286119a71719cf8006a8de1b8407d43
roberthsu2003 <roberthsu2003@gmail.com>
2024-12-13 11:07:30 +0800

    lesson18_2_4

--- lesson18/lesson18_2.py
+++ lesson18/lesson18_2.py
@@ -14,13 +14,11 @@ radio_data = [['pop','人口'],['lifeExp','平均壽命'],['gdpPercap','人均gd
 #selected要的資料
 selected_data = [{'value':value,'label':value} for value in df.country.unique()]
 
-data = [
-  {"date": "Mar 22", "Apples": 2890, "Oranges": 2338, "Tomatoes": 2452},
-  {"date": "Mar 23", "Apples": 2756, "Oranges": 2103, "Tomatoes": 2402},
-  {"date": "Mar 24", "Apples": 3322, "Oranges": 986, "Tomatoes": 1821},
-  {"date": "Mar 25", "Apples": 3470, "Oranges": 2108, "Tomatoes": 2809},
-  {"date": "Mar 26", "Apples": 3129, "Oranges": 1726, "Tomatoes": 2290}
-]
+#linechart要的資料
+# 
+dff = df[df.country == 'Taiwan']
+pop_diff = dff[['country', 'year', 'pop']]
+line_chart_data = pop_diff.to_dict('records')
 
 app.layout = dmc.MantineProvider(
     [
@@ -87,19 +85,17 @@ app.layout = dmc.MantineProvider(
         dmc.Container(
            dmc.LineChart(
             h=300,
-            dataKey="date",
-            data=data,
+            dataKey="year",
+            data=line_chart_data,
             series = [
-                {"name": "Apples", "color": "indigo.6"},
-                {"name": "Oranges", "color": "blue.6"},
-                {"name": "Tomatoes", "color": "teal.6"}
+                {"name": "pop", "color": "indigo.6"}
             ],
             curveType="linear",
             tickLine="xy",
             withXAxis=False,
             withDots=False,
             ),
-            mt=50 
+            my=50
         )
         
     ]
