68243afc369fb400af6212af1a839bbc2e72382c
roberthsu2003 <roberthsu2003@gmail.com>
2024-12-17 11:58:13 +0800

    lesson19_11

--- lesson18/auth/registration.py
+++ lesson18/auth/registration.py
@@ -1,6 +1,15 @@
 from . import auth
 from flask import render_template
+from flask_wtf import FlaskForm
+from wtforms import StringField,PasswordField,EmailField
+
+class RegistrationForm(FlaskForm):
+    username = StringField('username')
+    email = EmailField('Email Address')
+    password = PasswordField('New Password')
+    confirm =PasswordField('Repeat Password')
 
 @auth.route('/regist')
 def regist():
-    return render_template('auth/registration.j2')
+    form = RegistrationForm()
+    return render_template('auth/registration.j2',form=form)
