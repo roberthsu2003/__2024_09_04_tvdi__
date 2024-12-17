06bdb30b621bd3c997d9373cb482bec4dfb6f39a
roberthsu2003 <roberthsu2003@gmail.com>
2024-12-17 10:58:22 +0800

    lesson19_7

--- lesson18/lesson18.py
+++ lesson18/lesson18.py
@@ -1,8 +1,5 @@
 from flask import Flask,render_template,request,redirect,url_for
 import datasource
-from flask_wtf import FlaskForm
-from wtforms import EmailField,BooleanField,PasswordField,SubmitField
-from wtforms.validators import DataRequired,Length
 from werkzeug.middleware.dispatcher import DispatcherMiddleware
 from werkzeug.serving import run_simple
 import secrets
@@ -51,16 +48,7 @@ def pricing():
                             total_pages=total_pages,
                             page = page) 
 
-class MyForm(FlaskForm):
-    email_field = EmailField("Email address",validators=[DataRequired("必需要有資料")])
-    password_field = PasswordField("請輸入密碼",validators=[DataRequired("必需要有資料"),Length(5,10)])
-    epaper_field = BooleanField("訂閱電子報")
-    submit_field = SubmitField("確定送出")
 
-@app.route("/login",methods=['POST','GET'])
-def login():
-    myForm = MyForm()
-    return render_template('login.j2',myform = myForm)
 
 @app.route("/about")
 def about():
